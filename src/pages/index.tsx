ddec9256636fcbf53fdda5f02b8bced5778e2e8c
Billy Joe R. Santos <billyjoersantos@outlook.com>
2021-04-21 00:30:49 -0300

    Added requests to JSON server / day 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
-import { Header } from "../components/Header";
+import { useEffect } from "react"
 
-export default function Home() {
+export default function Home(props) {
+  // SPA EXAMPLE
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //   .then(response => response.json)
+  //   .then(data => console.log(data));
+  // }, []);
+
   return (
+    <>
     <h1>Index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+// SSR EXAMPLE
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = response.json();
+
+//   return { 
+//     props: {
+//       episodes: data
+//     } 
+//   }
+    
+// }
+
+// SSG EXAMPLE
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = response.json();
+
+  return { 
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+    
 }
